e3e310074e0f186ea5e2b6a1878b04cff4d27ab0
smirnovdm <sdm.01@mail.ru>
2022-08-16 23:54:08 +0300

    main menu changes

---
+++ src/main/java/com/example/controllers/AccountMenuController.java
@@ -0,0 +1,12 @@
+package com.example.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+import java.util.Objects;
+
+public class AccountMenuController {
+
+}
--- src/main/java/com/example/controllers/MainMenuController.java
+++ src/main/java/com/example/controllers/MainMenuController.java
@@ -1,5 +1,43 @@
 package com.example.controllers;
 
-public class MainMenuController {
+import com.example.db.DataBaseController;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 
+import java.util.Objects;
+
+public class MainMenuController {
+    @FXML
+    TextField bookSearchField;
+    @FXML
+    ListView<String> mainListView;
+
+    public void searchBookOrAuthor(ActionEvent e) {
+        String requestString = bookSearchField.getText();
+        DataBaseController dataBaseController = new DataBaseController();
+    }
+
+    private int findLevenshteinDistance(String a, String b) {
+        Objects.requireNonNull(a);
+        Objects.requireNonNull(b);
+        int[][] dp = new int[a.length() + 1][b.length() + 1];
+        for (int i = 0; i < a.length() + 1; i++) {
+            for (int j = 0; j < b.length() + 1; j++) {
+                if (i == 0) {
+                    dp[i][j] = j;
+                } else if (j == 0) {
+                    dp[i][j] = i;
+                } else if (a.charAt(i - 1) == b.charAt(j - 1)){
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]);
+                    dp[i][j] = Math.min(dp[i][j], dp[i - 1][i - 1]);
+                    dp[i][j]++;
+                }
+            }
+        }
+        return dp[a.length()][b.length()];
+    }
 }
